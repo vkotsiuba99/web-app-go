1c84eeea7453c626c2d9efdcb66afbef9ed6af78
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-06-14 12:31:04 +0300

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package web_app_go
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello world!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
