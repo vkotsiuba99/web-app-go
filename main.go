52ec890489a0c47f14050fe07b9bdd05ce056d6c
vkotsiuba99 <92172262+vkotsiuba99@users.noreply.github.com>
2022-06-14 12:33:13 +0300

    added gorilla/mux

--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package web_app_go
 
 import (
 	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/goodbye", goodbyeHandler).Methods("GET")
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello world!")
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+func goodbyeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Goodbye world!")
 }
